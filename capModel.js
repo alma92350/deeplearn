257eaa97de7a556b1d2b2eccb9ae2b96f92ecd85
alma92350 <almaatla@gmail.com>
2018-03-21 21:41:04 +0100

    increase conv weights

--- capModel.js
+++ capModel.js
@@ -1,7 +1,5 @@
 ///////////////////////////////
 // Network Parameters
-num_hidden_1 = 100; //# 1st layer num features
-num_hidden_2 = 200; //# 2nd layer num features (the latent dim)
 num_input = 0;
 TRAIN_STEPS = 5000;
 LEARNING_RATE = 0.01;
@@ -15,12 +13,12 @@ var GLOBAL_STEP = 0;
 const optimizer = dl.train.momentum(LEARNING_RATE,MOMENTUM);
 
 // Variables that we want to optimize
-const conv1OutputDepth = 16;
+const conv1OutputDepth = 26;
 var conv1Weights = dl.variable(
     	dl.randomNormal([5, 5, 1, conv1OutputDepth], 0, 0.1) );
 
 const conv2InputDepth = conv1OutputDepth;
-const conv2OutputDepth = 32;
+const conv2OutputDepth = 52;
 var conv2Weights = dl.variable(
     	dl.randomNormal([5, 5, conv2InputDepth, conv2OutputDepth], 0, 0.1));
 
