85542af92c6f885f397cdf502350bf1fac3c0af7
alma92350 <almaatla@gmail.com>
2018-03-23 23:06:50 +0100

    improvements

--- capModel1.js
+++ capModel1.js
@@ -2,6 +2,7 @@
 // added FILTER_SIZE
 // increased conv weights
 // add a fully connected layer at input
+// store model variables to local storage
 ///////////////////////////////
 // Network Parameters
 ///////////////////////////////
@@ -22,27 +23,79 @@ const optimizer = dl.train.momentum(LEARNING_RATE,MOMENTUM);
 // Variables that we want to optimize
 
 const hiddenInputLayerSize = 32;
-var fullyConnectedWeights_Input = dl.variable(
-    	dl.randomNormal(
-		      [IMAGE_SIZE*IMAGE_SIZE, hiddenInputLayerSize*hiddenInputLayerSize], 0,
-		      1 / Math.sqrt(IMAGE_SIZE*IMAGE_SIZE)));
-var fullyConnectedBias_input = dl.variable(dl.zeros([hiddenInputLayerSize*hiddenInputLayerSize]));
+var fullyConnectedWeights_Input;
+var fullyConnectedBias_input;
 
 const FILTER_SIZE=6;
 const conv1OutputDepth = 32;
-var conv1Weights = dl.variable(
-    	dl.randomNormal([FILTER_SIZE, FILTER_SIZE, 1, conv1OutputDepth], 0, 0.1) );
+var conv1Weights;
 
 const conv2InputDepth = conv1OutputDepth;
 const conv2OutputDepth = 64;
-var conv2Weights = dl.variable(
-    	dl.randomNormal([FILTER_SIZE, FILTER_SIZE, conv2InputDepth, conv2OutputDepth], 0, 0.1));
+var conv2Weights;
 
-var fullyConnectedWeights = dl.variable(
-    	dl.randomNormal(
-		      [8 * 8 * conv2OutputDepth, LABELS_SIZE], 0,
-		      1 / Math.sqrt(8 * 8 * conv2OutputDepth)));
-var fullyConnectedBias = dl.variable(dl.zeros([LABELS_SIZE]));
+var fullyConnectedWeights;
+var fullyConnectedBias;
+
+/////////////////////
+function initializeModelVariables(){
+	fullyConnectedWeights_Input = dl.variable(
+		  	dl.randomNormal(
+				    [IMAGE_SIZE*IMAGE_SIZE, hiddenInputLayerSize*hiddenInputLayerSize], 0,
+				    1 / Math.sqrt(IMAGE_SIZE*IMAGE_SIZE)));
+	fullyConnectedBias_input = dl.variable(dl.zeros([hiddenInputLayerSize*hiddenInputLayerSize]));
+
+	conv1Weights = dl.variable(
+		  	dl.randomNormal([FILTER_SIZE, FILTER_SIZE, 1, conv1OutputDepth], 0, 0.1) );
+
+	conv2Weights = dl.variable(
+		  	dl.randomNormal([FILTER_SIZE, FILTER_SIZE, conv2InputDepth, conv2OutputDepth], 0, 0.1));
+
+	fullyConnectedWeights = dl.variable(
+		  	dl.randomNormal(
+				    [8 * 8 * conv2OutputDepth, LABELS_SIZE], 0,
+				    1 / Math.sqrt(8 * 8 * conv2OutputDepth)));
+	fullyConnectedBias = dl.variable(dl.zeros([LABELS_SIZE]));
+}
+
+initializeModelVariables();
+
+const modelName = 'model1';
+function saveModelVariablesToLocalStorage(){
+	var modelVariables = [fullyConnectedWeights_Input.dataSync(),
+												fullyConnectedBias_input.dataSync(),
+												conv1Weights.dataSync(),
+												conv2Weights.dataSync(),
+												fullyConnectedWeights.dataSync(),
+												fullyConnectedBias.dataSync()];
+	//localStorage[modelName] = modelVariables;
+	return modelVariables;
+}
+
+function restoreModelVariablesFromLocalStorage(){
+	var modelVariables = JSON.parse(localStorage[modelName]);
+	var i=0;
+	fullyConnectedWeights_Input = dl.variable(dl.tensor(modelVariables[i++],
+																[IMAGE_SIZE*IMAGE_SIZE, hiddenInputLayerSize*hiddenInputLayerSize],
+																'float32'));
+	fullyConnectedBias_input = dl.variable(dl.tensor(modelVariables[i++],
+															[hiddenInputLayerSize*hiddenInputLayerSize],
+															'float32'));
+															
+	conv1Weights = dl.variable(dl.tensor(modelVariables[i++],
+									[FILTER_SIZE, FILTER_SIZE, 1, conv1OutputDepth],
+									'float32'));
+	conv2Weights = dl.variable(dl.tensor(modelVariables[i++],
+									[FILTER_SIZE, FILTER_SIZE, conv2InputDepth, conv2OutputDepth],
+									'float32'));
+									
+	fullyConnectedWeights = dl.variable(dl.tensor(modelVariables[i++],
+													[8 * 8 * conv2OutputDepth, LABELS_SIZE],
+													'float32'));
+	fullyConnectedBias = dl.variable(dl.tensor(modelVariables[i++],
+												[LABELS_SIZE],
+												'float32'));
+}
 
 /////////////////////
 // Loss function
